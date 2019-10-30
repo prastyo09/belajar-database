b092de01944915294b18decdfe4c1dfc5c82b109
RAHMATPRASTYO20 <rahmat.praztyo.20@gmail.com>
2019-10-30 20:02:00 +0700

    ditambahkan komen untuk mempermudah memahami script

--- karyawan/koneksi.php
+++ karyawan/koneksi.php
@@ -1,3 +1,4 @@
+<!-- untuk menghubungkan php dengan mysql database -->
 <?php
 $koneksi=mysqli_connect("localhost","root","");
 mysqli_select_db($koneksi,"apkaryawan_12tkj4_rahmat") or die ("database tidak bisa dibuka");
