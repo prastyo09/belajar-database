c7083df0e8a8981ae275e1641767ae83e3618a87
RAHMATPRASTYO20 <rahmat.praztyo.20@gmail.com>
2019-10-30 20:28:53 +0700

    menambahkan file form_input.php

---
+++ karyawan/form_input.php
@@ -0,0 +1,9 @@
+<div align="center">INPUT DATA</div>
+<form id="1" name="form1" method="post" action="proses_tambah.php">
+    <table width="545" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#999999">
+        <tr bgcolor="#00ff00">
+            <td width="90">Nama</td>
+            <td width="344"><input name="nama" id="nama" type="text"/>></td>
+        </tr>
+    </table>
+</form>
