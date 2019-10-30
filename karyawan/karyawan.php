c5ce0b49fa0ff058d947ff5431f469c8cf842d4e
prastyo09 <simamatads@gmail.com>
2019-10-30 19:50:17 +0700

    first commit

---
+++ karyawan/karyawan.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Si Mamat</title>
+</head>
+<body>
+    <p>DATA KARYAWAN</p>
+    <br>
+    <table width="627" border="1">
+        <body>
+            <tr>
+                <td>Nomor</td>
+                <td>Nama</td>
+                <td>Alamat</td>
+                <td>Jabatan</td>
+            </tr>
+            <?php
+            include"koneksi.php";
+            $kayawan=mysqli_query($koneksi,"SELECT * FROM tbkaryawan");
+            while($tampil=mysqli_fetch_array($kayawan)) {
+            ?>
+            <tr>
+                <td><?=$tampil["no"]?></td>
+                <td><?=$tampil["nama"]?></td>
+                <td><?=$tampil["alamat"]?></td>
+                <td><?=$tampil["jabatan"]?></td>
+            </tr>
+            <?php } ?>
+        </body>
+    </table>
+</body>
+</html>
---
+++ karyawan/koneksi.php
@@ -0,0 +1,4 @@
+<?php
+$koneksi=mysqli_connect("localhost","root","");
+mysqli_select_db($koneksi,"apkaryawan_12tkj4_rahmat") or die ("database tidak bisa dibuka");
+?>
